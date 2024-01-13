25a2ed3f38581d6377e9ef7c4ff4029860109427
Jonathan Cormier <jonathan@cormier.co>
2024-01-13 14:16:35 -0400

    Update symup.sh and add neovim configs
    Update symup.sh to handle .config subdirectories.
    Add neovim configs to .config/neovim directory.

--- symup.sh
+++ symup.sh
@@ -44,22 +44,49 @@ do
       continue
     fi
 
-    # Check for existing symlink, create and/or update symlink in home directory
-    if [[ -h "$ETARGET" && ($(readlink "$ETARGET") == "$EDOTFILE") ]]; then
-      echo "\x1B[92m$ETARGET is linked to your dotfiles.\x1B[39m"
-    elif [[ -f "$ETARGET" && $(checksum "$ETARGET" | awk '{print $2}') == $(checksum "$EBASE" | awk '{print $2}') ]]; then
-      echo "\x1B[93m$ETARGET exists and was identical to your dotfile. Overriding with symlink.\x1B[39m"
+    # Check for ~/.config directory
+    if [ "$EBASE" = ".config" ]; then
+	echo -e "\x1B[92m$ETARGET is a directory. Symlinking subdirectories.\x1B[39m"
+	
+	# Loop through subdirectories of ~/.config
+	for SUBENTRY in "$ENTRY"/*
+	do
+	    SUBBASE=$(basename "$SUBENTRY")
+	    SUBTARGET="$HOME/.config/$SUBBASE"
+	    SUBDOTFILE="$DOTFILES/.config/$SUBBASE"
 
-      symlink "$EDOTFILE" "$HOME"
-    elif [[ -a "$ETARGET" ]]; then
-      read -p "$ETARGET exists and differs from your dotfile. Override? [yN] " REPLY
+	    # Check for existing symlink, create and/or update symlink in home directory
+	    if [[ -h "$SUBTARGET" && ($(readlink "$SUBTARGET") == "$SUBDOTFILE") ]]; then
+		echo -e "\x1B[92m$SUBTARGET is linked to your dotfiles.\x1B[39m"
+	    elif [[ -a "$SUBTARGET" ]]; then
+		read -p "$SUBTARGET exists and differs from your dotfile. Override? [yN] " REPLY
 
-      # Check answer
-      if [[ "$REPLY" =~ ^([yY]*)$ ]]; then
-        symlink "$EDOTFILE" "$HOME"
-      fi
+		# Check answer
+		if [[ "$REPLY" =~ ^([yY]*)$ ]]; then
+		    symlink "$SUBDOTFILE" "$HOME/.config"
+		fi
+	    else
+		echo -e "\x1B[92m$SUBTARGET does not exist. Linking to its dotfile.\x1B[39m"
+		symlink "$SUBDOTFILE" "$HOME/.config"
+	    fi
+	done
     else
-      echo "\x1B[92m$ETARGET does not exist. Linking to it's dotfile.\x1B[39m"
-      symlink "$EDOTFILE" "$HOME"
+	# Check for existing symlink, create and/or update symlink in home directory
+	if [[ -h "$ETARGET" && ($(readlink "$ETARGET") == "$EDOTFILE") ]]; then
+	    echo -e "\x1B[92m$ETARGET is linked to your dotfiles.\x1B[39m"
+	elif [[ -f "$ETARGET" && $(checksum "$ETARGET" | awk '{print $2}') == $(checksum "$EBASE" | awk '{print $2}') ]]; then
+	    echo -e "\x1B[93m$ETARGET exists and was identical to your dotfile. Overriding with symlink.\x1B[39m"
+	    symlink "$EDOTFILE" "$HOME"
+	elif [[ -a "$ETARGET" ]]; then
+	    read -p "$ETARGET exists and differs from your dotfile. Override? [yN] " REPLY
+
+	    # Check answer
+	    if [[ "$REPLY" =~ ^([yY]*)$ ]]; then
+		symlink "$EDOTFILE" "$HOME"
+	    fi
+	else
+	    echo -e "\x1B[92m$ETARGET does not exist. Linking to its dotfile.\x1B[39m"
+	    symlink "$EDOTFILE" "$HOME"
+	fi
     fi
 done
