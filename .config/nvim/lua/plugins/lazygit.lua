749d9faf8a0ba3d505dfebc9039f69a330a7f7c6
Jonathan Cormier <jonathan@cormier.co>
2024-02-11 09:26:36 -0400

    Add lazygit.nvim keymap and Telescope ext

--- .config/nvim/lua/plugins/lazygit.lua
+++ .config/nvim/lua/plugins/lazygit.lua
@@ -1,4 +1,12 @@
 return {
   "kdheepak/lazygit.nvim",
-  dependencies = { "nvim-lua/plenary.nvim" }
+  dependencies = {
+    "nvim-telescope/telescope.nvim",
+    "nvim-lua/plenary.nvim"
+  },
+  init = function()
+    require("telescope").load_extension("lazygit")
+
+    km.set("n", "<leader>gg", "<cmd>LazyGit<CR>", { noremap = true, silent = true })
+  end
 }
