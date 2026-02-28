2520f629b3a400f58c8a49701471a65faae81417
Jonathan Cormier <jonathan@cormier.co>
2026-02-27 20:18:11 -0400

    refactor(nvim): migrate treesitter config from setup() to opts

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,32 +1,31 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = {
-	"elixir",
-	"lua",
-	"python",
-	"typescript",
-	"css",
-	"html",
-	"json",
-	"yaml",
-	"javascript",
-	"c",
-	"rust",
-	"go",
-	"gomod",
-	"gosum"
-      },
-      sync_install = false,
-      auto_install = true,
-      indent = {
-	enable = true
-      },
-      highlight = {
-	enable = true,
-	additional_vim_regex_highlighting = { "markdown" }
-      }
-    })
-  end
+  build = ":TSUpdate",
+  opts = {
+    ensure_installed = {
+      "elixir",
+      "lua",
+      "python",
+      "typescript",
+      "css",
+      "html",
+      "json",
+      "yaml",
+      "javascript",
+      "c",
+      "rust",
+      "go",
+      "gomod",
+      "gosum",
+    },
+    sync_install = false,
+    auto_install = true,
+    indent = {
+      enable = true,
+    },
+    highlight = {
+      enable = true,
+      additional_vim_regex_highlighting = { "markdown" },
+    },
+  },
 }
