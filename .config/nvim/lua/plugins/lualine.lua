c2da125cc1055e08223861a8937d2b564f15e014
Jonathan Cormier <jonathan@cormier.co>
2026-03-14 21:42:54 +0000

    feat(nvim): add jjtrack plugin and show change id in lualine
    - Add jjtrack dependency to display jj change IDs in the statusline
      and update plugin lockfile with latest versions.

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,16 +1,33 @@
 return {
   "nvim-lualine/lualine.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
+  dependencies = {
+	  "nvim-tree/nvim-web-devicons",
+	  "lucasadelino/jjtrack",
+  },
   config = function()
-    require("lualine").setup {
-      options = {
-	theme = "zenwritten",
-	globalstatus = true,
-	extensions = {
-	  "neo-tree",
-	  "man"
+	require("jjtrack").setup()
+
+	local function jj_change()
+		local s = vim.b.jjtrack_summary
+		if not s then return "" end
+		local prefix = s.change_id_prefix or ""
+		local rest = s.change_id_rest or ""
+		if prefix == "" then return "" end
+		return prefix .. rest
+	end
+
+	require("lualine").setup {
+		options = {
+			theme = "zenwritten",
+			globalstatus = true,
+			extensions = {
+				"neo-tree",
+				"man"
+			},
+		},
+		sections = {
+			lualine_b = { jj_change, "diff", "diagnostics" }
+		},
 	}
-      }
-    }
-  end
+  end,
 }
